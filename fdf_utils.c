d3c3a2c8179726a6639252a32ff2985a30063e6b
cmetee-b <claudinebitee@gmail.com>
2025-01-19 22:15:18 +0100

    cmmit debutparsing

--- fdf_utils.c
+++ fdf_utils.c
@@ -3,10 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   fdf_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmetee-b <cmetee-b@student.42.fr>          +#+  +:+       +#+        */
+/*   By: asiatik <asiatik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 15:56:35 by cmetee-b          #+#    #+#             */
-/*   Updated: 2025/01/14 15:56:36 by cmetee-b         ###   ########.fr       */
+/*   Updated: 2025/01/19 22:14:43 by asiatik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	free_map(int **map, int height)
+{
+	int	i;
+
+	if (map)
+	{
+		i = 0;
+		while (i < height)
+		{
+			free(map[i]);
+			i++;
+		}
+		free(map);
+	}
+}
+
+int	key_press(int keycode, void *param)
+{
+	void	**params;
+
+	params = (void **)param;
+	if (keycode == 65307) // ESC key on Linux
+	{
+		mlx_destroy_window(params[0], params[1]);
+		exit(0);
+	}
+	return (0);
+}
