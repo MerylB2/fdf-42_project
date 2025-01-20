faca4c9f25a2de8b961266b73677db90c5898e4d
cmetee-b <claudinebitee@gmail.com>
2025-01-20 14:58:52 +0100

    fdf modif avec struct

--- includes/fdf.h
+++ includes/fdf.h
@@ -5,29 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asiatik <asiatik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/19 21:41:17 by asiatik           #+#    #+#             */
-/*   Updated: 2025/01/19 22:04:54 by asiatik          ###   ########.fr       */
+/*   Created: 2025/01/20 14:44:58 by asiatik           #+#    #+#             */
+/*   Updated: 2025/01/20 14:49:04 by asiatik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* ---- includes/fdf.h ---- */
-
-#ifndef FDF_H
-# define FDF_H
-# include <mlx.h>
-# include <math.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-# define WINDOW_WIDTH 1200
-# define WINDOW_HEIGHT 800
-# define ZOOM_DEFAULT 30
-
-
-int ft_parse_map(char *filename, int **map_values, int *width, int *height);
-void ft_draw_map(int **map_values, int width, int height);
-int ft_handle_key(int key, void *param);
-int ft_close_window(void *param);
-
-#endif
--- includes/macros.h
+++ includes/macros.h
@@ -1,40 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf_utils.c                                        :+:      :+:    :+:   */
+/*   macros.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asiatik <asiatik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 15:56:35 by cmetee-b          #+#    #+#             */
-/*   Updated: 2025/01/19 22:15:01 by asiatik          ###   ########.fr       */
+/*   Created: 2025/01/20 14:44:41 by asiatik           #+#    #+#             */
+/*   Updated: 2025/01/20 14:49:04 by asiatik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	free_map(int **map, int height)
-{
-	int	i;
-
-	if (map)
-	{
-		i = 0;
-		while (i < height)
-		{
-			free(map[i]);
-			i++;
-		}
-		free(map);
-	}
-}
-
-int	key_press(int keycode, void *param)
-{
-	void	**params;
-
-	params = (void **)param;
-	if (keycode == 65307) // ESC key on Linux
-	{
-		mlx_destroy_window(params[0], params[1]);
-		exit(0);
-	}
-	return (0);
-}
